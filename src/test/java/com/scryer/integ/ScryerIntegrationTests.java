619eeb42284ab6c4b286ad1a61e8fb8d1dc3bf7a
reslix <huashengz@gmail.com>
2022-02-17 23:41:59 -0800

    add integ test for authentication

---
+++ src/test/java/com/scryer/integ/ScryerIntegrationTests.java
@@ -0,0 +1,83 @@
+package com.scryer.integ;
+
+import com.scryer.endpoint.EndpointApplication;
+import com.scryer.endpoint.Greeting;
+import com.scryer.model.ddb.UserSecurityModel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.Base64Utils;
+import org.springframework.util.MultiValueMap;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
+
+import java.nio.charset.StandardCharsets;
+
+@EnableAutoConfiguration
+@SpringBootTest(classes = {EndpointApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ScryerIntegrationTests {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    private final PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+    @MockBean
+    private DynamoDbTable<UserSecurityModel> userSecurityModelTable;
+
+    @Test
+    void testLogin() {
+        UserSecurityModel userSecurityModel = UserSecurityModel.builder()
+                                                               .username("test")
+                                                               .passwordHash(passwordEncoder.encode("test"))
+                                                               .build();
+        Mockito.when(userSecurityModelTable.getItem(ArgumentMatchers.any(Key.class)))
+               .thenReturn(userSecurityModel);
+        webTestClient.get()
+                     .uri("/api/hello")
+                     .accept(MediaType.APPLICATION_JSON)
+                     .exchange()
+                     .expectStatus()
+                     .isUnauthorized()
+                     .expectBody();
+        MultiValueMap<String, ResponseCookie> cookies =
+                webTestClient.post()
+                             .uri("/login")
+                             .header(HttpHeaders.AUTHORIZATION, "Basic " +
+                                                                Base64Utils.encodeToString("test:test".getBytes(
+                                                                        StandardCharsets.UTF_8)))
+                             .exchange()
+                             .expectStatus().isOk()
+                             .returnResult(Object.class).getResponseCookies();
+
+        webTestClient.get()
+                     .uri("/api/hello")
+                     .cookie("accessToken", cookies.toSingleValueMap().get("accessToken").getValue())
+                     .cookie("refreshToken", cookies.toSingleValueMap().get("refreshToken").getValue())
+                     .accept(MediaType.APPLICATION_JSON)
+                     .exchange()
+                     .expectStatus()
+                     .isOk()
+                     .expectBody().returnResult().getResponseCookies().toString();
+
+        webTestClient.get()
+                     .uri("/api/hello")
+                     .accept(MediaType.APPLICATION_JSON)
+                     .exchange()
+                     .expectStatus()
+                     .isUnauthorized()
+                     .expectBody();
+
+    }
+}
