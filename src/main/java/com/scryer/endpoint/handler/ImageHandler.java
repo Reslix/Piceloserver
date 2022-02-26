5fd55b54b233b4b26425d91113d3a50c13d71753
reslix <huashengz@gmail.com>
2022-02-26 10:13:35 -0800

    refactor image resize and put it on separate scheduler

--- src/main/java/com/scryer/endpoint/handler/ImageHandler.java
+++ src/main/java/com/scryer/endpoint/handler/ImageHandler.java
@@ -1,12 +1,10 @@
 package com.scryer.endpoint.handler;
 
-import com.scryer.endpoint.service.FolderService;
-import com.scryer.endpoint.service.ImageSrcService;
-import com.scryer.model.ddb.BaseIdentifier;
-import com.scryer.model.ddb.FolderModel;
-import com.scryer.model.ddb.ImageSrcModel;
 import com.scryer.endpoint.security.JWTManager;
-import org.springframework.core.io.ClassPathResource;
+import com.scryer.endpoint.service.ImageResizeService;
+import com.scryer.endpoint.service.ImageService;
+import com.scryer.model.ddb.BaseIdentifier;
+import com.scryer.model.ddb.ImageSrcModel;
 import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -23,22 +21,21 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Map;
 
 @Service
-public class ImageSrcHandler {
+public class ImageHandler {
 
-    private final ImageSrcService imageSrcService;
-    private final FolderService folderService;
+    private final ImageService imageService;
+    private final ImageResizeService imageResizeService;
     private final JWTManager jwtManager;
 
-    public ImageSrcHandler(final ImageSrcService imageSrcService,
-                           final FolderService folderService,
-                           final JWTManager jwtManager) {
-        this.imageSrcService = imageSrcService;
-        this.folderService = folderService;
+    public ImageHandler(final ImageService imageService,
+                        final ImageResizeService imageResizeService,
+                        final JWTManager jwtManager) {
+        this.imageService = imageService;
+        this.imageResizeService = imageResizeService;
         this.jwtManager = jwtManager;
     }
 
@@ -47,7 +44,7 @@ public class ImageSrcHandler {
     //Delete images
     public Mono<ServerResponse> getImagesByFolder(final ServerRequest request) {
         String folderId = request.pathVariable("folderId");
-        return imageSrcService.getImageSrcForFolderFromTable(folderId).collectList()
+        return imageService.getImageSrcForFolderFromTable(folderId).collectList()
                 .flatMap(imageSrcs -> ServerResponse.ok()
                         .contentType(MediaType.APPLICATION_JSON)
                         .body(BodyInserters.fromValue(imageSrcs)));
@@ -60,7 +57,11 @@ public class ImageSrcHandler {
             var multivalueMono = request.multipartData().map(MultiValueMap::toSingleValueMap);
             var typeStringMono = multivalueMono.map(map -> map.get("type").content())
                     .flatMap(bufferFlux -> DataBufferUtils.join(bufferFlux)
-                            .map(buffer -> new String(buffer.asByteBuffer().array())));
+                            .map(buffer -> new String(buffer.asByteBuffer().array())))
+                    .map(type -> {
+                        var split = type.split("/");
+                        return new String[] {type, split[0], split[1]};
+                    });
 
             var nameStringMono = multivalueMono.map(map -> map.get("name").content())
                     .flatMap(bufferFlux -> DataBufferUtils.join(bufferFlux)
@@ -70,33 +71,63 @@ public class ImageSrcHandler {
                     .flatMap(bufferFlux -> DataBufferUtils.join(bufferFlux)
                             .map(buffer -> new String(buffer.asByteBuffer().array())));
 
-            var imageMono = multivalueMono.map(map -> map.get("image").content())
+            var fullImageMono = multivalueMono.map(map -> map.get("image").content())
                     .flatMap(bufferFlux -> DataBufferUtils.join(bufferFlux)
                             .map(buffer -> buffer.asByteBuffer().array()));
 
-            var fullImageSrcMono = Mono.zip(nameStringMono, imageMono, (name, image) -> writeFullImage(image, name));
+            // convert size
+            var thumbnailMono = fullImageMono
+                    .zipWith(typeStringMono)
+                    .flatMap(tuple -> imageResizeService.getThumbnailImage(tuple.getT1(), tuple.getT2()));
 
-            var thumbnailImageSrcMono =
-                    Mono.zip(Mono.zip(nameStringMono, imageMono, typeStringMono)
-                                     .map(tuple -> writeThumbnailImage(tuple.getT2(), tuple.getT1(), tuple.getT3())),
-                             nameStringMono,
-                             typeStringMono,
-                             folderIdMono,
-                             imageSrcService.getUniqueId(),
-                             fullImageSrcMono)
-                            .flatMap(tuple -> createThumbnailImageSrc(tuple.getT1(),
-                                                                      tuple.getT2(),
-                                                                      tuple.getT3(),
-                                                                      tuple.getT4(),
-                                                                      tuple.getT5(),
-                                                                      userId,
-                                                                      tuple.getT6()));
+            // get the image IDs for both images - reserve their spots in the table.
+            var thumbnailIdMono = imageService.getUniqueId()
+                    .map(id -> ImageSrcModel.builder().id(id).build())
+                    .flatMap(imageService::addToImageSrcTable).map(ImageSrcModel::getId);
 
+            // write images to s3
+            var thumbnailLocation = Mono.zip(thumbnailIdMono, thumbnailMono, typeStringMono)
+                    .map(t3 -> imageService.addImagesToS3(t3.getT1(), "thumbnail", t3.getT2(), t3.getT3()))
+                    .flatMap(response -> response)
+                    .doOnError(error -> {
+                        thumbnailIdMono.map(id -> ImageSrcModel.builder().id(id).build())
+                                .map(imageService::deleteImageSrcFromTable);
+                    });
 
-            return thumbnailImageSrcMono.flatMap(imageSrc1 -> imageSrcService.updateImageSrcTable(imageSrc1))
-                    .flatMap(imageSrc -> ServerResponse.ok()
+            var fullImageLocation = Mono.zip(thumbnailIdMono, fullImageMono, typeStringMono)
+                    .map(t3 -> imageService.addImagesToS3(t3.getT1(), "full", t3.getT2(), t3.getT3()))
+                    .flatMap(response -> response)
+                    .doOnError(error -> {
+                        thumbnailIdMono.map(id -> ImageSrcModel.builder().id(id).build())
+                                .map(imageService::deleteImageSrcFromTable);
+                    });
+
+            // get imageSrc for both
+            var thumbnailSrcMono = Mono.zip(thumbnailIdMono,
+                                            nameStringMono,
+                                            typeStringMono,
+                                            thumbnailLocation,
+                                            folderIdMono,
+                                            fullImageLocation)
+                    .flatMap(t6 -> createImageSrc(t6.getT1(),
+                                                  t6.getT2(),
+                                                  t6.getT3(),
+                                                  "thumbnail",
+                                                  t6.getT4(),
+                                                  t6.getT5(),
+                                                  userId,
+                                                  "full",
+                                                  t6.getT6()))
+                    // For whatever reason method reference does not work
+                    .flatMap(imageSrc -> imageService.updateImageSrcTable(imageSrc));
+
+            // return thumbnail imageSrc
+            // fullImageSrcMono.then(..) leads to fullImageSrcMono not running so I probably don't understand it at all
+            return thumbnailSrcMono.flatMap(imageSrc -> ServerResponse.ok()
                             .contentType(MediaType.APPLICATION_JSON)
-                            .body(BodyInserters.fromValue(imageSrc)));
+                            .body(BodyInserters.fromValue(imageSrc)))
+                    .switchIfEmpty(ServerResponse.status(HttpStatus.BAD_REQUEST).build());
+
         }).switchIfEmpty(ServerResponse.status(HttpStatus.UNAUTHORIZED).build());
     }
 
@@ -104,68 +135,28 @@ public class ImageSrcHandler {
         return Mono.empty();
     }
 
-    private Mono<ImageSrcModel> createThumbnailImageSrc(final String path,
-                                                        final String name,
-                                                        final String type,
-                                                        final String folderId,
-                                                        final String imageId,
-                                                        final String userId,
-                                                        final String fullImagePath) {
+    private Mono<ImageSrcModel> createImageSrc(final String imageId,
+                                               final String name,
+                                               final String[] type,
+                                               final String size,
+                                               final String path,
+                                               final String folderId,
+                                               final String userId,
+                                               final String alternateSize,
+                                               final String alternateSource) {
         long currentTime = System.currentTimeMillis();
         return Mono.just(ImageSrcModel.builder()
                                  .id(imageId)
                                  .userId(userId)
-                                 .type(type)
+                                 .type(type[0])
                                  .name(name)
-                                 .size("thumbnail")
-                                 .source(new BaseIdentifier("static", path))
+                                 .size(size)
+                                 .source(new BaseIdentifier("url", path))
                                  .createDate(currentTime)
                                  .lastModified(currentTime)
                                  .parentFolderId(folderId)
-                                 .alternateSizes(Map.of("full", new BaseIdentifier("static", fullImagePath)))
+                                 .alternateSizes(Map.of(alternateSize, new BaseIdentifier("url", alternateSource)))
                                  .build());
 
-    }
-
-    private String writeThumbnailImage(final byte[] image, final String filename, final String type) {
-        try {
-            BufferedImage original = ImageIO.read(new ByteArrayInputStream(image));
-            var height = original.getHeight();
-            var width = original.getWidth();
-            double ratio;
-            BufferedImage resized;
-            if (width < height) {
-                ratio = 400.0 / height;
-                var targetWidth = Double.valueOf(width * ratio).intValue();
-                resized = new BufferedImage(targetWidth, 400, original.getType());
-                Graphics2D graphics2D = resized.createGraphics();
-                graphics2D.drawImage(original, 0, 0, targetWidth, 400, null);
-                graphics2D.dispose();
-            } else {
-                ratio = 400.0 / width;
-                var targetHeight = Double.valueOf(height * ratio).intValue();
-                resized = new BufferedImage(400, targetHeight, original.getType());
-                Graphics2D graphics2D = resized.createGraphics();
-                graphics2D.drawImage(original, 0, 0, 400, targetHeight, null);
-                graphics2D.dispose();
-            }
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            ImageIO.write(resized, type.split("/")[1], out);
-            Files.write(Path.of("build/classes/java/main/static/thumbnail/" + filename), out.toByteArray());
-            return "thumbnail/" + filename;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private String writeFullImage(final byte[] image, final String filename) {
-        try {
-            Files.write(Path.of("build/classes/java/main/static/full/" + filename), image);
-            return "full/" + filename;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
--- src/main/java/com/scryer/endpoint/service/ImageService.java
+++ src/main/java/com/scryer/endpoint/service/ImageService.java
@@ -5,21 +5,29 @@ import com.scryer.util.IdGenerator;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
-import software.amazon.awssdk.enhanced.dynamodb.Expression;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
 import software.amazon.awssdk.enhanced.dynamodb.model.PutItemEnhancedRequest;
 import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
 import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest;
 import software.amazon.awssdk.enhanced.dynamodb.model.UpdateItemEnhancedRequest;
-import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Service
-public class ImageSrcService {
+public class ImageService {
     private final DynamoDbTable<ImageSrcModel> imageSrcTable;
+    private final S3Client s3Client;
+    private final String s3BucketName;
 
-    public ImageSrcService(final DynamoDbTable<ImageSrcModel> imageSrcTable) {
+    public ImageService(final DynamoDbTable<ImageSrcModel> imageSrcTable,
+                        final S3Client s3Client,
+                        final String s3BucketName) {
         this.imageSrcTable = imageSrcTable;
+        this.s3Client = s3Client;
+        this.s3BucketName = s3BucketName;
     }
 
     public Mono<ImageSrcModel> getImageSrcFromTable(final String imageSrcId) {
@@ -48,13 +56,31 @@ public class ImageSrcService {
     }
 
     public Mono<ImageSrcModel> addToImageSrcTable(final ImageSrcModel imageSrc) {
-        return Mono.just(imageSrcTable.putItemWithResponse(PutItemEnhancedRequest.builder(ImageSrcModel.class)
-                                                                   .item(imageSrc)
-                                                                   .build()).attributes());
+        return Mono.justOrEmpty(imageSrcTable.putItemWithResponse(PutItemEnhancedRequest.builder(ImageSrcModel.class)
+                                                                          .item(imageSrc)
+                                                                          .build()).attributes())
+                .switchIfEmpty(getImageSrcFromTable(imageSrc.getId()));
+    }
+
+    public Mono<String> addImagesToS3(final String id, final String size, final byte[] image, final String[] type) {
+        var key = size + "/" + id + "." + type[2];
+        var request = PutObjectRequest.builder()
+                .bucket(s3BucketName)
+                .key(key)
+                .contentType(type[0]).build();
+        var body = RequestBody.fromBytes(image);
+
+        return Mono.justOrEmpty(s3Client.putObject(request, body))
+                .then(Mono.just(GetUrlRequest.builder().bucket(s3BucketName).key(key).build()))
+                .map(urlRequest -> s3Client.utilities().getUrl(urlRequest).toExternalForm());
+    }
+
+    public Mono<ImageSrcModel> deleteImageSrcFromTable(final ImageSrcModel imageSrc) {
+        return Mono.justOrEmpty(imageSrcTable.deleteItem(imageSrc));
     }
 
     public Mono<String> getUniqueId() {
-        return Mono.fromCallable(() -> IdGenerator.uniqueIdForTable(imageSrcTable, false));
+        return Mono.just(IdGenerator.uniqueIdForTable(imageSrcTable, false));
     }
 }
 
