56c564c3252501ebd56540c40a1c08da3cc7e837
reslix <huashengz@gmail.com>
2022-03-04 12:27:04 -0800

    add stubs for comparison operations

---
+++ src/main/java/com/scryer/endpoint/service/ComparisonService.java
@@ -0,0 +1,7 @@
+package com.scryer.endpoint.service;
+
+/**
+ * Hold methods for processing comparisons
+ */
+public class ComparisonService {
+}
---
+++ src/main/java/com/scryer/endpoint/service/FilterService.java
@@ -0,0 +1,15 @@
+package com.scryer.endpoint.service;
+
+/**
+ * Hold methods for processing filtering
+ */
+public class FilterService {
+
+    /**
+     * For constructing a k-regular graph (fully connected, k-colorable)
+     * If k=2m is even, put all the vertices around a circle, and join each to its m nearest neighbors on either side.
+     *
+     * If k=2m+1 is odd, and n is even, put the vertices on a circle, join each to its m nearest neighbors on each side, and also to the
+     * vertex directly opposite.
+     */
+}
