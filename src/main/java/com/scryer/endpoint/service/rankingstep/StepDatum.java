046ee1e28fa6e924df8b8b23254e723104aa5b90
reslix <huashengz@gmail.com>
2022-03-14 21:32:31 -0700

    fix StepDatum model

--- src/main/java/com/scryer/endpoint/service/rankingstep/StepDatum.java
+++ src/main/java/com/scryer/endpoint/service/rankingstep/StepDatum.java
@@ -7,5 +7,5 @@ import java.util.Map;
 
 @JsonSerialize
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public record StepDatum(String id, String name, Integer rating, Map<String, String> meta) {
+public record StepDatum(String id, String name, Integer rating, Map<String, Object> meta) {
 }
